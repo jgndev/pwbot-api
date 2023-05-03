6089e9c5332c4fa4602571d13e70d3e4ee901546
Jeremy Novak <olepunchy@gmail.com>
2023-05-03 14:19:10 -0500

    Updated passwordService

--- api/services/passwordService.ts
+++ api/services/passwordService.ts
@@ -5,26 +5,22 @@ const generatePassword = (options: PasswordRequest): PasswordResponse => {
     const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const lowercase = 'abcdefghijklmnopqrstuvwxyz';
     const numbers = '0123456789';
-    const specialCharacters = '!@#$%^&*-_=+<>?;:[]{}(),./|';
-
-    // Default the options that weren't specified by the caller
-    // options.length = options.length === 0 || null || undefined ? 12 : options.length;
-    // options.useLowercase = options.useLowercase === null || undefined ? true : options.useLowercase;
-    // options.useUppercase = options.useUppercase === null || undefined ? true : options.useUppercase;
-    // options.useSpecial = options.useSpecial === null || undefined ? true : options.useSpecial;
-    // options.useNumbers = options.useNumbers === null || undefined? true : options.useNumbers;
+    const special = '!@#$%^&*-_=+<>?;:[]{}(),./|';
 
     let characters = '';
 
     if (options.useLowercase) characters += lowercase;
     if (options.useUppercase) characters += uppercase;
     if (options.useNumbers) characters += numbers;
-    if (options.useSpecial) characters += specialCharacters;
+    if (options.useSpecial) characters += special;
 
     let password = '';
 
-    // Clamp the length to 12 if the caller passed a length less than 6 or greater than 64
-    options.length = options.length < 6 || options.length > 64 ? 12 : options.length;
+    // Clamp to between 6 and 64 characters for a generated password.
+    // Less than 6 characters is not a great password
+    // More than 64 characters would be a bit wild to handle in a UI
+    options.length = options.length < 6 ? 6 : options.length;
+    options.length = options.length > 64 ? 64 : options.length;
 
     for (let i = 0; i < options.length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
