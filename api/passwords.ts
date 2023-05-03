475cf22db921128755daeddb28bd5c954f967cbe
Jeremy Novak <olepunchy@gmail.com>
2023-05-03 14:23:27 -0500

    Updated for CORS

--- api/passwords.ts
+++ api/passwords.ts
@@ -3,9 +3,26 @@ import {PasswordRequest} from "./models/passwordRequest";
 import {PasswordResponse} from "./models/passwordResponse";
 import generatePassword from "./services/passwordService";
 
-export default (request: VercelRequest, response: VercelResponse): void => {
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,POST')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = (request: VercelRequest, response: VercelResponse)=> {
     const options: PasswordRequest = request.body;
     const password:PasswordResponse = generatePassword(options);
 
     response.json(password)
 }
+
+module.exports = allowCors(handler);
